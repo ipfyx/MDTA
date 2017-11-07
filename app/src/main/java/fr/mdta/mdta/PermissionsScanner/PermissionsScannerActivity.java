488923d8cee5b8ce467c6c2a058105cc425900b6
Baptiste POLVE <baptiste.polve@telecom-sudparis.eu>
2017-11-07 10:15:10 +0100

    add a user interface skeleton for permission scanner

---
+++ app/src/main/java/fr/mdta/mdta/PermissionsScanner/PermissionsScannerActivity.java
@@ -0,0 +1,15 @@
+package fr.mdta.mdta.PermissionsScanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.mdta.mdta.R;
+
+public class PermissionsScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions_scanner);
+    }
+}
