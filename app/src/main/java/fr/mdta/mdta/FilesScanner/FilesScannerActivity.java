d62cf4012e82845d2fedc2a1ff207b5a7c7c9d92
manwefm <florian.martin@telecom-sudparis.eu>
2017-12-04 17:02:02 +0100

    new methods to extract system apps and non system apps in 2 different list

--- app/src/main/java/fr/mdta/mdta/FilesScanner/FilesScannerActivity.java
+++ app/src/main/java/fr/mdta/mdta/FilesScanner/FilesScannerActivity.java
@@ -1,28 +1,40 @@
 package fr.mdta.mdta.FilesScanner;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import eu.chainfire.libsuperuser.Shell;
-import fr.mdta.mdta.FilesScanner.CommandFactory;
 
 import fr.mdta.mdta.R;
 
 public class FilesScannerActivity extends AppCompatActivity {
+
+    List<ApplicationInfo> installedApplications = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_files_scanner);
 
+        installedApplications = this.getPackageManager().getInstalledApplications(PackageManager.GET_SHARED_LIBRARY_FILES);
+
         boolean suAvailable = Shell.SU.available();
+
+        Log.d("system",getListNonSystemApps().toString());
 
         /**
          * https://stackoverflow.com/questions/2634991/android-1-6-android-view-windowmanagerbadtokenexception-unable-to-add-window
          */
         if ( suAvailable ) {
-            CommandFactory.execCommand("ls",this);
+            //CommandFactory.execCommand("ls",this);
         }
         else {
             TextView tv = (TextView) findViewById(R.id.sample_text);
@@ -37,4 +49,25 @@ public class FilesScannerActivity extends AppCompatActivity {
     protected String getSuVersionInternal() {
         return Shell.SU.version(true);
     }
+
+    protected List<ApplicationInfo> getListSystemApps() {
+        List<ApplicationInfo> systemApps = new ArrayList<ApplicationInfo>();
+        for (int i = 0; i < installedApplications.size(); i++) {
+            if ( ( installedApplications.get(i).flags & ApplicationInfo.FLAG_SYSTEM ) != 0 ) {
+                systemApps.add(installedApplications.get(i));
+            }
+        }
+        return systemApps;
+    }
+
+    protected List<ApplicationInfo> getListNonSystemApps() {
+        List<ApplicationInfo> nonSystemApps = new ArrayList<ApplicationInfo>();
+        for (int i = 0; i < installedApplications.size(); i++) {
+            if (  ( installedApplications.get(i).flags & ApplicationInfo.FLAG_SYSTEM ) == 0 ) {
+                nonSystemApps.add(installedApplications.get(i));
+            }
+        }
+        return nonSystemApps;
+    }
+
 }
