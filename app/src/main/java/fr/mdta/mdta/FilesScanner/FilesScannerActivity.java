69e43789a5033974b0a49699293128765c0a6e49
manwefm <florian.martin@telecom-sudparis.eu>
2017-12-04 11:36:58 +0100

    new package FilesScanner with a new activity and layout

---
+++ app/src/main/java/fr/mdta/mdta/FilesScanner/FilesScannerActivity.java
@@ -0,0 +1,16 @@
+package fr.mdta.mdta.FilesScanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.mdta.mdta.R;
+
+public class FilesScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files_scanner);
+
+    }
+}
--- app/src/main/java/fr/mdta/mdta/MainActivity.java
+++ app/src/main/java/fr/mdta/mdta/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import fr.mdta.mdta.FilesScanner.FilesScannerActivity;
 import fr.mdta.mdta.PermissionsScanner.PermissionsScannerActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, PermissionsScannerActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button buttonFilesScanner = (Button) findViewById(R.id.filesScanner);
+        buttonFilesScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FilesScannerActivity.class);
                 startActivity(i);
             }
         });
