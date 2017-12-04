abbe52df5c6cea74dfe28362c2e7f42311b20048
manwefm <florian.martin@telecom-sudparis.eu>
2017-12-04 15:05:23 +0100

    removed unecessary asynctask class

--- app/src/main/java/fr/mdta/mdta/SignaturesScanner/SignaturesScannerActivity.java
+++ app/src/main/java/fr/mdta/mdta/SignaturesScanner/SignaturesScannerActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,8 +23,7 @@ import eu.chainfire.libsuperuser.Shell;
 public class SignaturesScannerActivity extends AppCompatActivity {
 
     private TextView mResultTextView;
-
-    boolean suAvailable = false;
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +33,13 @@ public class SignaturesScannerActivity extends AppCompatActivity {
         mResultTextView = (TextView) findViewById(R.id.displaySignatureScanner);
 
         final Button launchButton = (Button) findViewById(R.id.launchButton);
-        (new Startup()).setContext(this).execute();
+
         launchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     ArrayList<PackageSignaturesInfo> result = SignaturesInfoFactory.getInstalledPackages(SignaturesScannerActivity.this);
                     //From there you can have access to the good object to make something with signature
-
                 } catch (CertificateException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -48,67 +47,5 @@ public class SignaturesScannerActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    private class Startup extends AsyncTask<Void, Void, Void> {
-        private ProgressDialog dialog = null;
-        private Context context = null;
-        private boolean suAvailable = false;
-        private String suVersion = null;
-        private String suVersionInternal = null;
-        private List<String> suResult = null;
-
-        public Startup setContext(Context context) {
-            this.context = context;
-            return this;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            // We're creating a progress dialog here because we want the user to wait.
-            // If in your app your user can just continue on with clicking other things,
-            // don't do the dialog thing.
-
-            dialog = new ProgressDialog(context);
-            dialog.setTitle("Scaning the app");
-            dialog.setMessage("Please wait");
-            dialog.setIndeterminate(true);
-            dialog.setCancelable(false);
-            dialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            // Let's do some SU stuff
-            suAvailable = Shell.SU.available();
-            if (suAvailable) {
-                suVersion = Shell.SU.version(false);
-                suVersionInternal = Shell.SU.version(true);
-                suResult = Shell.SU.run(new String[] {
-                        "mkdir -p /data/local/mdta",
-                        "touch /data/local/mdta/mdta"
-                });
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            dialog.dismiss();
-
-            // output
-            StringBuilder sb = (new StringBuilder()).
-                    append("Root? ").append(suAvailable ? "Yes" : "No").append((char)10).
-                    append("Version: ").append(suVersion == null ? "N/A" : suVersion).append((char)10).
-                    append("Version (internal): ").append(suVersionInternal == null ? "N/A" : suVersionInternal).append((char)10).
-                    append((char)10);
-            if (suResult != null) {
-                for (String line : suResult) {
-                    sb.append(line).append((char)10);
-                }
-            }
-            ((TextView)findViewById(R.id.displaySignatureScanner)).setText(sb.toString());
-        }
     }
 }
