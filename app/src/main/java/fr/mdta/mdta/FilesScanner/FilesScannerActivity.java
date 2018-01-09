a392dc2c2ecc69610c70e249c932b635d7f0bda4
manwefm <florian.martin@telecom-sudparis.eu>
2018-01-09 11:12:33 +0100

    there could be md5Digest in manifest

--- app/src/main/java/fr/mdta/mdta/FilesScanner/FilesScannerActivity.java
+++ app/src/main/java/fr/mdta/mdta/FilesScanner/FilesScannerActivity.java
@@ -335,7 +335,11 @@ public class FilesScannerActivity extends AppCompatActivity implements Callback 
                 String fileHash = entry.getValue().getValue("SHA-256-Digest");
                 if ( fileHash == null ) {
                     fileHash = entry.getValue().getValue("SHA1-Digest");
-                    verifySha1(filePath,fileHash,uid);
+                    if ( fileHash == null ){
+                        //MD5 or somethingElse ?
+                    } else {
+                        verifySha1(filePath,fileHash,uid);
+                    }
                 } else {
                     verifySha256(filePath,fileHash,uid);
                 }
