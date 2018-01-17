4c5d9d70ac0a6d20f1c08bd3c26e036767b0452b
Baptiste POLVE <baptiste.polve@telecom-sudparis.eu>
2018-01-17 20:21:34 +0100

    Update Main activity UI to access 4 differents possibilities

--- app/src/main/java/fr/mdta/mdta/MainActivity.java
+++ app/src/main/java/fr/mdta/mdta/MainActivity.java
@@ -5,50 +5,65 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import fr.mdta.mdta.PermissionsScanner.PermissionsScannerActivity;
-import fr.mdta.mdta.SignaturesScanner.SignaturesScannerActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    //UI components
+    private Button mScanApplicationsButton;
+    private Button mScanAllSystemButton;
+    private Button mScanSpecificAppButton;
+    private Button mCustomScanButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
-        Button button = (Button) findViewById(R.id.permissionsScanner);
-        button.setOnClickListener(new View.OnClickListener() {
+        //Retrieve UI components and set every related click option
+        mScanApplicationsButton = (Button) findViewById(R.id.scanApplications);
+        mScanApplicationsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, PermissionsScannerActivity.class);
-                startActivity(i);
+                Intent myIntent = new Intent(MainActivity.this, ScanActivity.class);
+                myIntent.putExtra(ScanActivity.KEY_TYPE_OF_SCAN, ScanActivity.TypeOfScan.APPLICATIONSSCAN);
+                startActivity(myIntent);
+                finish();
             }
         });
 
-        Button signatureButton = (Button) findViewById(R.id.signaturesScanner);
-        signatureButton.setOnClickListener(new View.OnClickListener() {
+        mScanAllSystemButton = (Button) findViewById(R.id.scanAllSystem);
+        mScanAllSystemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, SignaturesScannerActivity.class);
-                startActivity(i);
+                Intent myIntent = new Intent(MainActivity.this, ScanActivity.class);
+                myIntent.putExtra(ScanActivity.KEY_TYPE_OF_SCAN, ScanActivity.TypeOfScan.WHOLESYSTEMSCAN);
+                startActivity(myIntent);
+                finish();
             }
         });
+
+        mScanSpecificAppButton = (Button) findViewById(R.id.scanSpecificApp);
+        mScanSpecificAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, SpecifyAppActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
+
+        mCustomScanButton = (Button) findViewById(R.id.customScanButton);
+        mCustomScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, CustomScanActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
 }
