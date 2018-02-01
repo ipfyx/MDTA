732f728298632d5feba2d92dff20e1e51152246e
manwefm <florian.martin@telecom-sudparis.eu>
2018-02-01 12:03:02 +0100

    comments added

--- app/src/main/java/fr/mdta/mdta/Tools/CommandFactory.java
+++ app/src/main/java/fr/mdta/mdta/Tools/CommandFactory.java
@@ -16,25 +16,42 @@ import fr.mdta.mdta.Model.SimplifiedPackageInfo;
 
 public final class CommandFactory {
 
-    private static final int MAX_PROCESS_INTEGRITY = 5;
+    //private static final int MAX_PROCESS_INTEGRITY = 5;
+    //list of asynctask to process
     public static ArrayList<Command> listProcessIntegrity = new ArrayList<>();
 
-    public static int COUNT = 0;
+    //public static int COUNT = 0;
+
+    //path to unzip directory, set in constructor of DexScanner and IntegrityScanner
     public static String pathToApkUnzipFolder = "/data/local";
 
+    /**
+     * shell command to execute to unzip an apk
+     * @param callback
+     * @param appInfo
+     * @param my_uid
+     * @param SELinuxContext
+     * @param unzipDirectoryName
+     */
     public static void unzipCommand(Callback callback, SimplifiedPackageInfo appInfo, int
             my_uid, String SELinuxContext, String unzipDirectoryName) {
 
         String[] listCommand = new String[]{
+                //we move to pathtoApkUnzipFolder to know where we are
                 "cd " + pathToApkUnzipFolder,
+                //we clean the directory that could already exist
                 "rm -rRf " + pathToApkUnzipFolder + unzipDirectoryName + "_" + Integer.toString
                         (appInfo.getAppUid()),
+                //create the directory we unzip to
                 "mkdir -p " + pathToApkUnzipFolder + unzipDirectoryName + "_" + Integer
                         .toString(appInfo.getAppUid()),
+                //unzip the apk
                 "unzip " + appInfo.getApkSourceDir() + " -d " + pathToApkUnzipFolder + unzipDirectoryName + "_"
                         + Integer.toString(appInfo.getAppUid()),
+                //change owner of files
                 "chown -R " + my_uid + ":" + my_uid + " " + pathToApkUnzipFolder +
                         unzipDirectoryName + "_" + Integer.toString(appInfo.getAppUid()),
+                //change files context
                 "chcon -R " + SELinuxContext + " " + pathToApkUnzipFolder + unzipDirectoryName +
                         "_" + Integer.toString(appInfo.getAppUid())
                 /* "echo " + app.packageName + " " + Integer.toString(app.uid)+">> "+pathToApkUnzipFolder+"test",
@@ -50,6 +67,7 @@ public final class CommandFactory {
 
     public static void endScanApp(SimplifiedPackageInfo app, String unzipDirectoryName) {
 
+        //remove folder when scan app is done
         String[] listCommand = new String[]{
                 "cd /data/local",
                 "rm -rRf " + pathToApkUnzipFolder + unzipDirectoryName + "_" + Integer.toString(app
@@ -69,6 +87,12 @@ public final class CommandFactory {
         }
     }
 
+    /**
+     * Launch every asynTask in listProcess
+     * @param callback
+     * @param appInfo
+     */
+
     public static void launchVerification(Callback callback, SimplifiedPackageInfo appInfo) {
 
         //COUNT = 0;
@@ -85,15 +109,11 @@ public final class CommandFactory {
         }
     }
 
-    public static void changeDirectoryContext(Callback callback, Context context, String directoryPath, String SELinuxContext) {
 
-        String[] listCommand = new String[]{
-                "chcon -R " + SELinuxContext + " " + directoryPath
-        };
-        Command exec_command = new Command(callback, listCommand);
-        exec_command.execute(listCommand);
-    }
-
+    /**
+     * remove every possible folder where an app could have been unziped
+     * @param unzipDirectoryName
+     */
     public static void brutallyEndScanApp(String unzipDirectoryName) {
 
         String[] listCommand = new String[]{
@@ -105,6 +125,12 @@ public final class CommandFactory {
         exec_command.execute(listCommand);
     }
 
+
+    /**
+     * ugly method to get the exact name of *.SF and *.RSA
+     * @param pathToMETAINF
+     * @param callback
+     */
     public static void getCertNames(String pathToMETAINF,Callback callback) {
 
         String[] listCommand = new String[]{
